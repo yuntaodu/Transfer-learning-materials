3db9f9b4fae51ce922f581dae3a7382c11bbe09d
CaoKang <caokang@CaoKangdeMacBook-Pro.local>
2019-10-24 13:55:41 +0800

    feat: add experiment result for ARRLSB

--- discuss/week2_10-23/experiment/ARTL/code/run.m
+++ discuss/week2_10-23/experiment/ARTL/code/run.m
@@ -13,57 +13,66 @@ result = [];
 Accs = [];
 plotMatrix = [];
 % text dataset experiment
-% 
-% textDataSet = ["comp", "rec", "sci", "talk"];
-% textDataSplitNum = 36;
-% 
-% for i1 = 1:length(textDataSet)
-%     for i2 = i1:length(textDataSet)
-%         if i1 == i2
-%             continue
-%         end
-%         for index = 1:textDataSplitNum
-%             data = strcat(textDataSet(i1), '_vs_', textDataSet(i2), '_', num2str(index));
-%             options.data = data;
-%             load(strcat('../data/',data));
-% %             Accs = [];
-%             for i = 1:runs
-%                 [Acc,Cls,Alpha] = ARRLS(X_src,X_tar,Y_src,Y_tar,options);
-%                 Accs = [Accs;Acc(end)];
-%             end
-%             average = mean(Accs);
-%             stderr = std(Accs);
-%             result = [result;[average,stderr]];
-%         end
-%     end
-% end
+%
+textDataSet = ["comp_vs_talk"];
+%"comp_vs_rec" , "comp_vs_sci", "comp_vs_talk", "rec_vs_sci", "rec_vs_talk", "sci_vs_talk"
+textDataSplitNum = 36;
 
-% image dataset experiment
-
-for dataStr = {'USPS_vs_MNIST'}
-    for iDataset = 1:2
-        data = strcat(char(dataStr),'_',num2str(iDataset));
+for i1 = 1:length(textDataSet)
+    for index = 1:textDataSplitNum
+        data = strcat(textDataSet, '_', num2str(index));
         options.data = data;
-        
         load(strcat('data/',data));
         for j = 0:0.05:1
             options.Mu = j;
             [Acc,Cls,Alpha] = ARRLSB(X_src,X_tar,Y_src,Y_tar,options);
-%           [Acc,Cls,Alpha] = ARRLS(X_src,X_tar,Y_src,Y_tar,options);
-            Accs = [Accs;Acc(end)];
+            %           [Acc,Cls,Alpha] = ARRLS(X_src,X_tar,Y_src,Y_tar,options);
+%             Accs = [Accs;Acc(end)];
             
-            average = mean(Accs);
-            stderr = std(Accs);
-            result = [result;[average,stderr]];
+%             average = mean(Accs);
+%             stderr = std(Accs);
+%             result = [result;[average,stderr]];
             plotMatrix = [plotMatrix; [j, Acc(end)]];
+            
+            fprintf("Progress: %f \n", (((i1-1)*textDataSplitNum + index-1 + j) * 20)/(textDataSplitNum * length(textDataSet) * 20));
         end
+        
         plot(plotMatrix(:,1),plotMatrix(:,2));
         xlabel('Mu');
         ylabel('Accuracy');
         hold on;
         plotMatrix = [];
     end
+    
+    
 end
 
-% disp(Accs); 
+% image dataset experiment
+
+% for dataStr = {'USPS_vs_MNIST'}
+%     for iDataset = 1
+%         data = strcat(char(dataStr),'_',num2str(iDataset));
+%         options.data = data;
+%
+%         load(strcat('data/',data));
+%         for j = 0:0.05:1
+%             options.Mu = j;
+%             [Acc,Cls,Alpha] = ARRLSB(X_src,X_tar,Y_src,Y_tar,options);
+% %           [Acc,Cls,Alpha] = ARRLS(X_src,X_tar,Y_src,Y_tar,options);
+%             Accs = [Accs;Acc(end)];
+%
+%             average = mean(Accs);
+%             stderr = std(Accs);
+%             result = [result;[average,stderr]];
+%             plotMatrix = [plotMatrix; [j, Acc(end)]];
+%         end
+%         plot(plotMatrix(:,1),plotMatrix(:,2));
+%         xlabel('Mu');
+%         ylabel('Accuracy');
+%         hold on;
+%         plotMatrix = [];
+%     end
+% end
+
+% disp(Accs);
 % disp(plotMatrix);
